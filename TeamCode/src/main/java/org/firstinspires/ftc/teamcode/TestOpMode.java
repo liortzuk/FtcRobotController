2b75ada2b459a858650c9772006aff359ee3a89a
liortzuk <liortzuk@gmail.com>
2023-10-02 15:40:30 +0300

    FTC Controller setup (TeleOp)

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestOpMode.java
@@ -0,0 +1,23 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp
+public class TestOpMode extends OpMode {
+
+    @Override
+    protected void postInit() {
+
+    }
+
+    @Override
+    protected void run() {
+        runtime.reset();
+
+    }
+
+    @Override
+    protected void end() {
+
+    }
+}
